6e44083433a1461961517f93db72e1feaaf8e2e0
jagdishkonline@gmail.com <Jagdish@20>
2019-07-28 16:26:18 +0530

    SimpleClass Three Updated

---
+++ GitTestNew/src/test/java/com/gittestnew/GitTestNew/SimpleClassThree.java
@@ -0,0 +1,11 @@
+package com.gittestnew.GitTestNew;
+
+public class SimpleClassThree {
+	
+	public static void main(String args[] )
+	{
+		System.out.println("Hello World");
+		System.out.println("Merging File");
+	}
+
+}
