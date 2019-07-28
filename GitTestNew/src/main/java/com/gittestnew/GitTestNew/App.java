da2904fdcb66404429cf07ab168f77598a84e684
jagdishkonline@gmail.com <Jagdish@20>
2019-07-28 15:47:01 +0530

    SimpleClassTwo.java Updated

---
+++ GitTestNew/src/main/java/com/gittestnew/GitTestNew/App.java
@@ -0,0 +1,13 @@
+package com.gittestnew.GitTestNew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GitTestNew/src/test/java/com/gittestnew/GitTestNew/SimpleClassTwo.java
@@ -0,0 +1,12 @@
+package com.gittestnew.GitTestNew;
+
+public class SimpleClassTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My Project Updated");
+
+	}
+
+}
