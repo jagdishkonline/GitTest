2f5ced14f959681d4a8354326c4bd9ff8540a9fe
jagdishkonline@gmail.com <Jagdish@20>
2019-07-28 16:42:43 +0530

    SimpleClassFour Java Updated

---
+++ GitTestNew/src/test/java/com/gittestnew/GitTestNew/SmpleClassFour.java
@@ -0,0 +1,12 @@
+package com.gittestnew.GitTestNew;
+
+public class SmpleClassFour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("SimpleClass Four Java");
+	}
+
+}
